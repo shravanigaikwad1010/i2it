79233b21af43bbf1e6762b21eccd1d0c67048c20
shravanigaikwad1010 <shravanigaikwad1010@gmail.com>
2026-04-14 19:11:25 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,14 @@
+public class demo
+{
+    public static void main(String[] args)
+    {
+        int i=10;int j=2;int c;
+        c=i+j;
+        System.out.println("Hello");
+        System.out.println("i= "+i);
+        i++;
+        System.out.println("i= "+i);
+        System.out.println("c= "+c);
+    }
+};
+;
